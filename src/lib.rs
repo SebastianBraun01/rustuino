f24649ddbc16ebe0cdc32b1b7e41f5a453dd9bab
SebastianBraun01 <SebastianBraun-Ost@protonmail.com>
2021-08-14 14:44:21 +0200

    custrom panics

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@
 // Library includes ===============================================================================
 pub use cortex_m_rt::{entry, exception};
 pub use stm32f4::stm32f446::{NVIC, Interrupt, interrupt};
-pub use panic_halt as _;
 
 // Für benutzer
 pub use libm::*;
@@ -19,3 +18,16 @@ pub mod analog;
 pub mod time;
 pub mod uart;
 pub mod i2c;
+
+
+// Panic handler ==================================================================================
+use core::panic::PanicInfo;
+
+#[inline(never)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+  rtt_target::rprintln!("{}", info);
+  loop {
+    core::sync::atomic::compiler_fence(core::sync::atomic::Ordering::SeqCst);
+  }
+}
