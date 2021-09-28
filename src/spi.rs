b2c8d5ed76675a82950f9fe917221f61297942d5
SebastianBraun01 <SebastianBraun-Ost@protonmail.com>
2021-09-28 13:49:37 +0200

    spi better config

--- src/spi.rs
+++ src/spi.rs
@@ -1,9 +1,9 @@
 use crate::include::{stm_peripherals, SpiError, ProgError, SPI_DATA};
 use crate::gpio::{pin_mode, digital_write, GpioMode::AlternateFunction, GpioMode::Output};
 use heapless::FnvIndexMap;
+use rtt_target::rprintln;
 
 #[allow(non_camel_case_types)]
-#[derive(PartialEq, Eq)]
 pub enum ClockMode {
   MODE_0,
   MODE_1,
@@ -12,7 +12,6 @@ pub enum ClockMode {
 }
 
 #[allow(non_camel_case_types)]
-#[derive(PartialEq, Eq)]
 pub enum SpiMode {
   FULL_DUPLEX,
   HALF_DUPLEX,
@@ -20,7 +19,6 @@ pub enum SpiMode {
   SIMPLEX_OUTPUT
 }
 
-#[derive(PartialEq, Eq)]
 pub enum FrameFormat {
   MSBFIRST,
   LSBFIRST
@@ -65,7 +63,7 @@ impl SPI {
       1 => {
         let spi1 = &peripheral_ptr.SPI1;
         if rcc.apb2enr.read().spi1en().is_enabled() {
-          rtt_target::rprintln!("SPI{} is already configured! | SPI::new()", core);
+          rprintln!("SPI{} is already configured! | SPI::new()", core);
           return Err(SpiError::Prog(ProgError::PermissionDenied));
         }
         rcc.apb2enr.modify(|_, w| w.spi1en().enabled());
@@ -80,7 +78,7 @@ impl SPI {
       2 => {
         let spi2 = &peripheral_ptr.SPI2;
         if rcc.apb1enr.read().spi2en().is_enabled() == true {
-          rtt_target::rprintln!("SPI{} is already configured! | SPI::new()", core);
+          rprintln!("SPI{} is already configured! | SPI::new()", core);
           return Err(SpiError::Prog(ProgError::PermissionDenied));
         }
         rcc.apb1enr.modify(|_, w| w.spi2en().enabled());
@@ -95,7 +93,7 @@ impl SPI {
       3 => {
         let spi3 = &peripheral_ptr.SPI3;
         if rcc.apb1enr.read().spi3en().is_enabled() == true {
-          rtt_target::rprintln!("SPI{} is already configured! | SPI::new()", core);
+          rprintln!("SPI{} is already configured! | SPI::new()", core);
           return Err(SpiError::Prog(ProgError::PermissionDenied));
         }
         rcc.apb1enr.modify(|_, w| w.spi3en().enabled());
@@ -107,7 +105,7 @@ impl SPI {
           w.spe().enabled()
         });
       },
-      _ => panic!("SPI{} is not a valid core! | SPI::new()", core)
+      _ => unreachable!()
     };
 
     return Ok(Self {
@@ -143,30 +141,95 @@ impl SPI {
         spi3.cr1.reset();
         spi3.cr2.reset();
       },
-      _ => panic!("SPI{} is not a valid core! | .end()", self.core)
+      _ => unreachable!()
     };
   }
 
-  pub fn configure(&mut self, mode: SpiMode, clk: ClockMode, frame: FrameFormat, br: SpiBr) -> Result<(), ()> {
+  pub fn set_mode(&mut self, mode: SpiMode) -> Result<(), SpiError> {
     let peripheral_ptr = stm_peripherals();
 
-    if self.active == true {return Err(());}
+    if self.active == true {
+      rprintln!("Cannot configure SPI core while active! | .set_mode()");
+      return Err(SpiError::Prog(ProgError::PermissionDenied));
+    }
 
     match self.core {
       1 => {
         let spi1 = &peripheral_ptr.SPI1;
         spi1.cr1.modify(|_, w| w.spe().disabled());
-        if mode != SpiMode::FULL_DUPLEX {spi1.cr1.modify(|_, w| w.bidimode().set_bit());}
-        else {spi1.cr1.modify(|_, w| w.bidimode().clear_bit());}
-        if mode == SpiMode::SIMPLEX_INPUT {spi1.cr1.modify(|_, w| w.bidioe().clear_bit());}
-        else if mode == SpiMode::SIMPLEX_OUTPUT {spi1.cr1.modify(|_, w| w.bidioe().set_bit());}
+        match mode {
+          SpiMode::FULL_DUPLEX => spi1.cr1.modify(|_, w| w.bidimode().clear_bit()),
+          SpiMode::HALF_DUPLEX => spi1.cr1.modify(|_, w| w.bidimode().set_bit()),
+          SpiMode::SIMPLEX_INPUT => {
+            spi1.cr1.modify(|_, w| w.bidimode().set_bit());
+            spi1.cr1.modify(|_, w| w.bidioe().clear_bit());
+          },
+          SpiMode::SIMPLEX_OUTPUT => {
+            spi1.cr1.modify(|_, w| w.bidimode().set_bit());
+            spi1.cr1.modify(|_, w| w.bidioe().set_bit());
+          }
+        };
+        spi1.cr1.modify(|_, w| w.spe().enabled());
+      },
+      2 => {
+        let spi2 = &peripheral_ptr.SPI2;
+        spi2.cr1.modify(|_, w| w.spe().disabled());
+        match mode {
+          SpiMode::FULL_DUPLEX => spi2.cr1.modify(|_, w| w.bidimode().clear_bit()),
+          SpiMode::HALF_DUPLEX => spi2.cr1.modify(|_, w| w.bidimode().set_bit()),
+          SpiMode::SIMPLEX_INPUT => {
+            spi2.cr1.modify(|_, w| w.bidimode().set_bit());
+            spi2.cr1.modify(|_, w| w.bidioe().clear_bit());
+          },
+          SpiMode::SIMPLEX_OUTPUT => {
+            spi2.cr1.modify(|_, w| w.bidimode().set_bit());
+            spi2.cr1.modify(|_, w| w.bidioe().set_bit());
+          }
+        };
+        spi2.cr1.modify(|_, w| w.spe().enabled());
+      },
+      3 => {
+        let spi3 = &peripheral_ptr.SPI3;
+        spi3.cr1.modify(|_, w| w.spe().disabled());
+        match mode {
+          SpiMode::FULL_DUPLEX => spi3.cr1.modify(|_, w| w.bidimode().clear_bit()),
+          SpiMode::HALF_DUPLEX => spi3.cr1.modify(|_, w| w.bidimode().set_bit()),
+          SpiMode::SIMPLEX_INPUT => {
+            spi3.cr1.modify(|_, w| w.bidimode().set_bit());
+            spi3.cr1.modify(|_, w| w.bidioe().clear_bit());
+          },
+          SpiMode::SIMPLEX_OUTPUT => {
+            spi3.cr1.modify(|_, w| w.bidimode().set_bit());
+            spi3.cr1.modify(|_, w| w.bidioe().set_bit());
+          }
+        };
+        spi3.cr1.modify(|_, w| w.spe().enabled());
+      },
+      _ => unreachable!()
+    };
+
+    self.mode = mode;
+    return Ok(());
+  }
+
+  pub fn set_clk(&self, clk: ClockMode, br: SpiBr) -> Result<(), SpiError> {
+    let peripheral_ptr = stm_peripherals();
+
+    if self.active == true {
+      rprintln!("Cannot configure SPI core while active! | .set_clk()");
+      return Err(SpiError::Prog(ProgError::PermissionDenied));
+    }
+
+    match self.core {
+      1 => {
+        let spi1 = &peripheral_ptr.SPI1;
+        spi1.cr1.modify(|_, w| w.spe().disabled());
         match clk {
           ClockMode::MODE_0 => spi1.cr1.modify(|_, w| {w.cpol().clear_bit(); w.cpha().clear_bit()}),
           ClockMode::MODE_1 => spi1.cr1.modify(|_, w| {w.cpol().clear_bit(); w.cpha().set_bit()}),
           ClockMode::MODE_2 => spi1.cr1.modify(|_, w| {w.cpol().set_bit(); w.cpha().set_bit()}),
           ClockMode::MODE_3 => spi1.cr1.modify(|_, w| {w.cpol().set_bit(); w.cpha().clear_bit()}),
         };
-
         match br {
           SpiBr::DIV_2   => spi1.cr1.modify(|_, w| w.br().div2()),
           SpiBr::DIV_4   => spi1.cr1.modify(|_, w| w.br().div4()),
@@ -177,69 +240,110 @@ impl SPI {
           SpiBr::DIV_128 => spi1.cr1.modify(|_, w| w.br().div128()),
           SpiBr::DIV_256 => spi1.cr1.modify(|_, w| w.br().div256()),
         };
-
-        if frame == FrameFormat::MSBFIRST {spi1.cr1.modify(|_, w| w.lsbfirst().clear_bit());}
-        else {spi1.cr1.modify(|_, w| w.lsbfirst().set_bit());}
         spi1.cr1.modify(|_, w| w.spe().enabled());
       },
       2 => {
         let spi2 = &peripheral_ptr.SPI2;
         spi2.cr1.modify(|_, w| w.spe().disabled());
-        if mode != SpiMode::FULL_DUPLEX {spi2.cr1.modify(|_, w| w.bidimode().set_bit());}
-        else {spi2.cr1.modify(|_, w| w.bidimode().clear_bit());}
-        if mode == SpiMode::SIMPLEX_INPUT {spi2.cr1.modify(|_, w| w.bidioe().clear_bit());}
-        else if mode == SpiMode::SIMPLEX_OUTPUT {spi2.cr1.modify(|_, w| w.bidioe().set_bit());}
         match clk {
           ClockMode::MODE_0 => spi2.cr1.modify(|_, w| {w.cpol().clear_bit(); w.cpha().clear_bit()}),
           ClockMode::MODE_1 => spi2.cr1.modify(|_, w| {w.cpol().clear_bit(); w.cpha().set_bit()}),
           ClockMode::MODE_2 => spi2.cr1.modify(|_, w| {w.cpol().set_bit(); w.cpha().set_bit()}),
           ClockMode::MODE_3 => spi2.cr1.modify(|_, w| {w.cpol().set_bit(); w.cpha().clear_bit()}),
         };
-        if frame == FrameFormat::MSBFIRST {spi2.cr1.modify(|_, w| w.lsbfirst().clear_bit());}
-        else {spi2.cr1.modify(|_, w| w.lsbfirst().set_bit());}
+        match br {
+          SpiBr::DIV_2   => spi2.cr1.modify(|_, w| w.br().div2()),
+          SpiBr::DIV_4   => spi2.cr1.modify(|_, w| w.br().div4()),
+          SpiBr::DIV_8   => spi2.cr1.modify(|_, w| w.br().div8()),
+          SpiBr::DIV_16  => spi2.cr1.modify(|_, w| w.br().div16()),
+          SpiBr::DIV_32  => spi2.cr1.modify(|_, w| w.br().div32()),
+          SpiBr::DIV_64  => spi2.cr1.modify(|_, w| w.br().div64()),
+          SpiBr::DIV_128 => spi2.cr1.modify(|_, w| w.br().div128()),
+          SpiBr::DIV_256 => spi2.cr1.modify(|_, w| w.br().div256()),
+        };
         spi2.cr1.modify(|_, w| w.spe().enabled());
       },
       3 => {
         let spi3 = &peripheral_ptr.SPI3;
         spi3.cr1.modify(|_, w| w.spe().disabled());
-        if mode != SpiMode::FULL_DUPLEX {spi3.cr1.modify(|_, w| w.bidimode().set_bit());}
-        else {spi3.cr1.modify(|_, w| w.bidimode().clear_bit());}
-        if mode == SpiMode::SIMPLEX_INPUT {spi3.cr1.modify(|_, w| w.bidioe().clear_bit());}
-        else if mode == SpiMode::SIMPLEX_OUTPUT {spi3.cr1.modify(|_, w| w.bidioe().set_bit());}
         match clk {
           ClockMode::MODE_0 => spi3.cr1.modify(|_, w| {w.cpol().clear_bit(); w.cpha().clear_bit()}),
           ClockMode::MODE_1 => spi3.cr1.modify(|_, w| {w.cpol().clear_bit(); w.cpha().set_bit()}),
           ClockMode::MODE_2 => spi3.cr1.modify(|_, w| {w.cpol().set_bit(); w.cpha().set_bit()}),
           ClockMode::MODE_3 => spi3.cr1.modify(|_, w| {w.cpol().set_bit(); w.cpha().clear_bit()}),
         };
-        if frame == FrameFormat::MSBFIRST {spi3.cr1.modify(|_, w| w.lsbfirst().clear_bit());}
-        else {spi3.cr1.modify(|_, w| w.lsbfirst().set_bit());}
+        match br {
+          SpiBr::DIV_2   => spi3.cr1.modify(|_, w| w.br().div2()),
+          SpiBr::DIV_4   => spi3.cr1.modify(|_, w| w.br().div4()),
+          SpiBr::DIV_8   => spi3.cr1.modify(|_, w| w.br().div8()),
+          SpiBr::DIV_16  => spi3.cr1.modify(|_, w| w.br().div16()),
+          SpiBr::DIV_32  => spi3.cr1.modify(|_, w| w.br().div32()),
+          SpiBr::DIV_64  => spi3.cr1.modify(|_, w| w.br().div64()),
+          SpiBr::DIV_128 => spi3.cr1.modify(|_, w| w.br().div128()),
+          SpiBr::DIV_256 => spi3.cr1.modify(|_, w| w.br().div256()),
+        };
         spi3.cr1.modify(|_, w| w.spe().enabled());
       },
-      _ => panic!("SPI{} is not a valid core! | .configure()", self.core)
+      _ => unreachable!()
+
     };
 
-    self.mode = mode;
     return Ok(());
   }
 
-  pub fn add_slave(&mut self, pin: (char, u8), id: u8) -> Result<(), ()> {
+  pub fn set_frame_format(&self, frame: FrameFormat) -> Result<(), SpiError> {
+    let peripheral_ptr = stm_peripherals();
+
+    if self.active == true {
+      rprintln!("Cannot configure SPI core while active! | .set_frame_format()");
+      return Err(SpiError::Prog(ProgError::PermissionDenied));
+    }
+
+    match self.core {
+      1 => {
+        let spi1 = &peripheral_ptr.SPI1;
+        spi1.cr1.modify(|_, w| w.spe().disabled());
+        if let FrameFormat::MSBFIRST = frame {spi1.cr1.modify(|_, w| w.lsbfirst().clear_bit());}
+        else {spi1.cr1.modify(|_, w| w.lsbfirst().set_bit());}
+        spi1.cr1.modify(|_, w| w.spe().enabled());
+      },
+      2 => {
+        let spi2 = &peripheral_ptr.SPI2;
+        spi2.cr1.modify(|_, w| w.spe().disabled());
+        if let FrameFormat::MSBFIRST = frame {spi2.cr1.modify(|_, w| w.lsbfirst().clear_bit());}
+        else {spi2.cr1.modify(|_, w| w.lsbfirst().set_bit());}
+        spi2.cr1.modify(|_, w| w.spe().enabled());
+      },
+      3 => {
+        let spi3 = &peripheral_ptr.SPI3;
+        spi3.cr1.modify(|_, w| w.spe().disabled());
+        if let FrameFormat::MSBFIRST = frame {spi3.cr1.modify(|_, w| w.lsbfirst().clear_bit());}
+        else {spi3.cr1.modify(|_, w| w.lsbfirst().set_bit());}
+        spi3.cr1.modify(|_, w| w.spe().enabled());
+      },
+      _ => unreachable!()
+    };
+
+    return Ok(());
+  }
+
+  pub fn add_slave(&mut self, pin: (char, u8), id: u8) -> Result<(), SpiError> {
     if self.com_pins.contains(&pin) == true {
-      rtt_target::rprintln!("P{}{} is not available as an NSS pin! | .add_slave()", pin.0.to_uppercase(), pin.1);
-      return Err(());
+      rprintln!("P{}{} is not available as an NSS pin! | .add_slave()", pin.0.to_uppercase(), pin.1);
+      return Err(SpiError::Prog(ProgError::PermissionDenied));
     }
     else if self.nss.contains_key(&id) == true {
-      rtt_target::rprintln!("ID {} already registered for an NSS pin! | .add_slave()", id);
-      return Err(());
+      rprintln!("ID {} already registered for an NSS pin! | .add_slave()", id);
+      return Err(SpiError::Prog(ProgError::PermissionDenied));
     }
     else if self.nss.values().any(|&i| i == pin) == true {
-      rtt_target::rprintln!("P{}{} already registered as an NSS pin! | .add_slave()", pin.0.to_uppercase(), pin.1);
-      return Err(());
+      rprintln!("P{}{} already registered as an NSS pin! | .add_slave()", pin.0.to_uppercase(), pin.1);
+      return Err(SpiError::Prog(ProgError::PermissionDenied));
     }
 
     if self.nss.insert(id, pin).is_err() {
-      rtt_target::rprintln!("Cannot register more than 5 NSS pins! | .add_slave()");
-      return Err(());
+      rprintln!("Cannot register more than 5 NSS pins! | .add_slave()");
+      return Err(SpiError::Prog(ProgError::PermissionDenied));
     }
 
     pin_mode(pin, Output).expect("Could not configre pin as an NSS pin! | .add_slave()");
@@ -248,15 +352,15 @@ impl SPI {
     return Ok(());
   }
 
-  pub fn begin_transaction(&mut self, id: u8) -> Result<(), ()> {
+  pub fn begin_transaction(&mut self, id: u8) -> Result<(), SpiError> {
     if self.nss.contains_key(&id) == false {
-      rtt_target::rprintln!("ID {} not registered! | .begin_transaction()", id);
-      return Err(());
+      rprintln!("ID {} not registered! | .begin_transaction()", id);
+      return Err(SpiError::Prog(ProgError::InvalidConfiguration));
     }
 
     if self.active == true {
-      rtt_target::rprintln!("SPI already active! | .begin_transaction()");
-      return Err(());
+      rprintln!("SPI already active! | .begin_transaction()");
+      return Err(SpiError::Prog(ProgError::InvalidConfiguration));
     }
 
     digital_write(self.nss.get(&id).unwrap().clone(), false).expect("Could not set pin value! | .begin_transaction()");
@@ -269,12 +373,15 @@ impl SPI {
   pub fn write(&self, data: u8) -> Result<(), SpiError> {
     let peripheral_ptr = stm_peripherals();
 
-    if self.mode == SpiMode::SIMPLEX_INPUT {return Err(SpiError::Prog(ProgError::PermissionDenied));}
+    if let SpiMode::SIMPLEX_INPUT = self.mode {
+      rprintln!("Cannot send data in SIMPLEX_INPUT configuration! | .write()");
+      return Err(SpiError::Prog(ProgError::PermissionDenied));
+    }
 
     match self.core {
       1 => {
         let spi1 = &peripheral_ptr.SPI1;
-        if self.mode == SpiMode::HALF_DUPLEX {spi1.cr1.modify(|_, w| w.bidioe().set_bit());}
+        if let SpiMode::HALF_DUPLEX = self.mode {spi1.cr1.modify(|_, w| w.bidioe().set_bit());}
         while spi1.sr.read().txe().bit_is_clear() == true {
           if let Err(error) = scan_spi_error(spi1.sr.read().bits() as u16) {return Err(error);}
         }
@@ -282,7 +389,7 @@ impl SPI {
       },
       2 => {
         let spi2 = &peripheral_ptr.SPI2;
-        if self.mode == SpiMode::HALF_DUPLEX {spi2.cr1.modify(|_, w| w.bidioe().set_bit());}
+        if let SpiMode::HALF_DUPLEX = self.mode {spi2.cr1.modify(|_, w| w.bidioe().set_bit());}
         while spi2.sr.read().txe().bit_is_clear() == true {
           if let Err(error) = scan_spi_error(spi2.sr.read().bits() as u16) {return Err(error);}
         }
@@ -290,13 +397,13 @@ impl SPI {
       },
       3 => {
         let spi3 = &peripheral_ptr.SPI3;
-        if self.mode == SpiMode::HALF_DUPLEX {spi3.cr1.modify(|_, w| w.bidioe().set_bit());}
+        if let SpiMode::HALF_DUPLEX = self.mode {spi3.cr1.modify(|_, w| w.bidioe().set_bit());}
         while spi3.sr.read().txe().bit_is_clear() == true {
           if let Err(error) = scan_spi_error(spi3.sr.read().bits() as u16) {return Err(error);}
         }
         spi3.dr.write(|w| w.dr().bits(data.into()));
       },
-      _ => panic!("SPI{} is not a valid core! | .write()", self.core)
+      _ => unreachable!()
     };
 
     return Ok(());
@@ -306,12 +413,15 @@ impl SPI {
     let peripheral_ptr = stm_peripherals();
     let buffer: u8;
 
-    if self.mode == SpiMode::SIMPLEX_OUTPUT {return Err(SpiError::Prog(ProgError::PermissionDenied));}
+    if let SpiMode::SIMPLEX_OUTPUT = self.mode {
+      rprintln!("Cannot read data in SIMPLEX_OUTPUT configuration! | .read()");
+      return Err(SpiError::Prog(ProgError::PermissionDenied));
+    }
 
     match self.core {
       1 => {
         let spi1 = &peripheral_ptr.SPI1;
-        if self.mode == SpiMode::HALF_DUPLEX {spi1.cr1.modify(|_, w| w.bidioe().set_bit());}
+        if let SpiMode::HALF_DUPLEX = self.mode {spi1.cr1.modify(|_, w| w.bidioe().set_bit());}
         while spi1.sr.read().txe().bit_is_clear() == true {
           if let Err(error) = scan_spi_error(spi1.sr.read().bits() as u16) {return Err(error);}
         }
@@ -319,12 +429,12 @@ impl SPI {
         while spi1.sr.read().rxne().bit_is_clear() == true {
           if let Err(error) = scan_spi_error(spi1.sr.read().bits() as u16) {return Err(error);}
         }
-        if self.mode == SpiMode::HALF_DUPLEX {spi1.cr1.modify(|_, w| w.bidioe().clear_bit());}
+        if let SpiMode::HALF_DUPLEX = self.mode {spi1.cr1.modify(|_, w| w.bidioe().clear_bit());}
         buffer = spi1.dr.read().dr().bits() as u8;
       },
       2 => {
         let spi2 = &peripheral_ptr.SPI2;
-        if self.mode == SpiMode::HALF_DUPLEX {spi2.cr1.modify(|_, w| w.bidioe().set_bit());}
+        if let SpiMode::HALF_DUPLEX = self.mode {spi2.cr1.modify(|_, w| w.bidioe().set_bit());}
         while spi2.sr.read().txe().bit_is_clear() == true {
           if let Err(error) = scan_spi_error(spi2.sr.read().bits() as u16) {return Err(error);}
         }
@@ -332,12 +442,12 @@ impl SPI {
         while spi2.sr.read().rxne().bit_is_clear() == true {
           if let Err(error) = scan_spi_error(spi2.sr.read().bits() as u16) {return Err(error);}
         }
-        if self.mode == SpiMode::HALF_DUPLEX {spi2.cr1.modify(|_, w| w.bidioe().clear_bit());}
+        if let SpiMode::HALF_DUPLEX = self.mode {spi2.cr1.modify(|_, w| w.bidioe().clear_bit());}
         buffer = spi2.dr.read().dr().bits() as u8;
       },
       3 => {
         let spi3 = &peripheral_ptr.SPI3;
-        if self.mode == SpiMode::HALF_DUPLEX {spi3.cr1.modify(|_, w| w.bidioe().set_bit());}
+        if let SpiMode::HALF_DUPLEX = self.mode {spi3.cr1.modify(|_, w| w.bidioe().set_bit());}
         while spi3.sr.read().txe().bit_is_clear() == true {
           if let Err(error) = scan_spi_error(spi3.sr.read().bits() as u16) {return Err(error);}
         }
@@ -345,10 +455,10 @@ impl SPI {
         while spi3.sr.read().rxne().bit_is_clear() == true {
           if let Err(error) = scan_spi_error(spi3.sr.read().bits() as u16) {return Err(error);}
         }
-        if self.mode == SpiMode::HALF_DUPLEX {spi3.cr1.modify(|_, w| w.bidioe().clear_bit());}
+        if let SpiMode::HALF_DUPLEX = self.mode {spi3.cr1.modify(|_, w| w.bidioe().clear_bit());}
         buffer = spi3.dr.read().dr().bits() as u8;
       },
-      _ => panic!("SPI{} is not a valid core! | .read()", self.core)
+      _ => unreachable!()
     }
 
     return Ok(buffer);
@@ -388,7 +498,10 @@ fn check_spi(core: u8, sck: (char, u8), miso: (char, u8), mosi: (char, u8)) -> R
       else if SPI_DATA.s3_mosi.contains(&mosi) == false {return Err(SpiError::Prog(ProgError::InvalidConfiguration));}
       else {return Ok(6);}
     },
-    _ => panic!("SPI{} is not a valid core! | check_spi()", core)
+    _ => {
+      rprintln!("SPI{} is not a valid core! | check_spi()", core);
+      return Err(SpiError::Prog(ProgError::InvalidConfiguration));
+    }
   };
 }
 
