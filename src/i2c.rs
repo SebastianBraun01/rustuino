35cd469899afdd14575d84d82888f0d48eb9ff7e
SebastianBraun01 <SebastianBraun-Ost@protonmail.com>
2022-01-09 21:28:01 +0100

    i2c docs finished

--- src/i2c.rs
+++ src/i2c.rs
@@ -1,3 +1,32 @@
+//! This module contains everything that is used for I2C communication.
+//! 
+//! For information on whitch pins have UART capabilities, check [`I2C_MAP`](crate::include::I2C_MAP)
+//! 
+//! # Examples
+//! 
+//! ```no_run
+//! #![no_std]
+//! #![no_main]
+//! 
+//! use rustuino::*;
+//! 
+//! #[entry]
+//! fn main() -> ! {
+//!   // Configure the serial connection with a buffer size of 16 bytes.
+//!   let i2c = I2C::new::<16>(1, PB6, PB7, true).unwrap();
+//! 
+//!   loop {
+//!     // Send Data
+//!     i2c.beginTransmission(0x10);
+//!     i2c.write(1).unwrap();
+//!     i2c.write(2).unwrap();
+//!     i2c.write(3).unwrap();
+//!     i2c.endTransmission(true).unwrap();
+//!     delay(1000);   
+//!   }
+//! }
+//! ```
+
 use crate::include::{I2cError, ProgError, I2C_MAP, PIN_CONF};
 use crate::gpio::{pinmode_alternate_function, open_drain, set_bias, GpioBias::Pullup, Pin, AlternateFunction};
 use heapless::Vec;
@@ -6,18 +35,31 @@ use rtt_target::rprintln;
 const BUS_FREQ: u32 = 16000000;
 const I2C_FREQ: u32 = 100000;
 
+/// This struct represents a configured I2C peripheral.
 pub struct I2C<const N: usize> {
+  #[doc(hidden)]
   core: u8,
+  #[doc(hidden)]
   _scl_pin: Pin<AlternateFunction>,
+  #[doc(hidden)]
   _sda_pin: Pin<AlternateFunction>,
+  #[doc(hidden)]
   tx_buffer: Vec<u8, N>,
+  #[doc(hidden)]
   rx_buffer: Vec<u8, N>,
+  #[doc(hidden)]
   tx_addr: u8,
+  #[doc(hidden)]
   transmitting: bool
 }
 
 impl<const N: usize> I2C<N> {
-  pub fn new(core: u8, scl_pin: (char, u8), sda_pin: (char, u8), pullup: bool, addr: u8) -> Result<Self, ProgError> {
+  /// Configure a I2C connection with one of the internal I2C peripherals.
+  /// 
+  /// This Method expects the used I2C core, two [pin identifiers](crate::include::pins) for the scl and sda-pins
+  /// and if the internal pullup-resistors should be used as parameters and returns the [I2C Struct](crate::i2c::I2C).
+  /// Specitfy the buffer capapcity with the turbofish operator. Panics if the core or pins are already used or invalid.
+  pub fn new(core: u8, scl_pin: (char, u8), sda_pin: (char, u8), pullup: bool) -> Result<Self, ProgError> {
     let peripheral_ptr;
     unsafe {peripheral_ptr = stm32f4::stm32f446::Peripherals::steal();}
     let rcc = &peripheral_ptr.RCC;
@@ -72,7 +114,7 @@ impl<const N: usize> I2C<N> {
         i2c1.cr2.modify(|_, w| unsafe {w.freq().bits(BUS_FREQ as u8)});
         i2c1.ccr.modify(|_, w| unsafe {w.ccr().bits(ccr_t as u16)});
         i2c1.trise.write(|w| w.trise().bits(rise_t as u8));
-        if addr > 0 {i2c1.oar1.modify(|_, w| w.add().bits((addr << 1).into()));}
+        // if addr > 0 {i2c1.oar1.modify(|_, w| w.add().bits((addr << 1).into()));}
         i2c1.cr1.modify(|_, w| {
           w.ack().set_bit();
           w.pe().enabled()
@@ -88,7 +130,7 @@ impl<const N: usize> I2C<N> {
         i2c2.cr2.modify(|_, w| unsafe {w.freq().bits(BUS_FREQ as u8)});
         i2c2.ccr.modify(|_, w| unsafe {w.ccr().bits(ccr_t as u16)});
         i2c2.trise.write(|w| w.trise().bits(rise_t as u8));
-        if addr > 0 {i2c2.oar1.modify(|_, w| w.add().bits((addr << 1).into()));}
+        // if addr > 0 {i2c2.oar1.modify(|_, w| w.add().bits((addr << 1).into()));}
         i2c2.cr1.modify(|_, w| {
           w.ack().set_bit();
           w.pe().enabled()
@@ -104,7 +146,7 @@ impl<const N: usize> I2C<N> {
         i2c3.cr2.modify(|_, w| unsafe {w.freq().bits(BUS_FREQ as u8)});
         i2c3.ccr.modify(|_, w| unsafe {w.ccr().bits(ccr_t as u16)});
         i2c3.trise.write(|w| w.trise().bits(rise_t as u8));
-        if addr > 0 {i2c3.oar1.modify(|_, w| w.add().bits((addr << 1).into()));}
+        // if addr > 0 {i2c3.oar1.modify(|_, w| w.add().bits((addr << 1).into()));}
         i2c3.cr1.modify(|_, w| {
           w.ack().set_bit();
           w.pe().enabled()
@@ -124,6 +166,7 @@ impl<const N: usize> I2C<N> {
     });
   }
 
+  /// Deacitivates the I2C peripheral and destroys the struct, freeing the core and pins.
   pub fn end(self) {
     let peripheral_ptr;
     unsafe {peripheral_ptr = stm32f4::stm32f446::Peripherals::steal();}
@@ -159,14 +202,18 @@ impl<const N: usize> I2C<N> {
       },
       _ => panic!("I2C{} is not a valid core! | I2C::new()", self.core)
     };
+
+    drop(self);
   }
 
+  /// Initiate transmission to a slave with entered address.
   pub fn begin_transmission(&mut self, addr: u8) {
     self.transmitting = true;
     self.tx_addr = addr << 1;
     self.tx_buffer.clear();
   }
 
+  /// Add a byte that is send to the slave. Returns an error-enum if transmittion is not initiated or the tx buffer is full.
   pub fn write(&mut self, data: u8) -> Result<(), ProgError> {
     if !self.transmitting {return Err(ProgError::PermissionDenied);}
 
@@ -174,6 +221,9 @@ impl<const N: usize> I2C<N> {
     else {return Ok(());}
   }
 
+  /// Burst tranfers the bytes in the tx buffer to the slave. Specify if a stop bit should be send. Normally this
+  /// should be true. It can be false if you want to communicate with multiple slaves simultaniously.
+  /// Returns an error-enum if problems with the connection are detected.
   pub fn end_transmission(&mut self, stop: bool) -> Result<(), I2cError> {
     let peripheral_ptr;
     unsafe {peripheral_ptr = stm32f4::stm32f446::Peripherals::steal();}
@@ -240,6 +290,10 @@ impl<const N: usize> I2C<N> {
     return Ok(());
   }
 
+  /// Request a number of bytes from a slave with the specified address. Recieved bytes are stored in the rx buffer.
+  /// Specify if a stop bit should be send. Normally this should be true. It can be false if you want to communicate
+  /// with multiple slaves simultaniously.
+  /// Returns an error-enum if the buffer cannot hold the number of bytes or a problem with the connection is detected.
   pub fn request_bytes(&mut self, addr: u8, nbytes: u8, stop: bool) -> Result<usize, I2cError> {
     let peripheral_ptr;
     unsafe {peripheral_ptr = stm32f4::stm32f446::Peripherals::steal();}
@@ -442,19 +496,23 @@ impl<const N: usize> I2C<N> {
     return Ok(self.rx_buffer.len());
   }
 
+  /// Returns the number of bytes in the rx buffer.
   pub fn available(&self) -> usize {
     return self.rx_buffer.len();
   }
 
+  /// Read a byte from the rx buffer. Returns None if buffer is empty.
   pub fn read(&mut self) -> Option<u8> {
     return self.rx_buffer.pop();
   }
 
+  /// Set the clock frequency to a specific value in Hz. Default is 100kHz. Values lower than 10kHz or higher than
+  /// 400kHz are not recomendet.
   pub fn set_clock(&self, clk: u32) -> Result<(), I2cError> {
     let peripheral_ptr;
     unsafe {peripheral_ptr = stm32f4::stm32f446::Peripherals::steal();}
 
-    if clk < 10000 || clk > 400000 {
+    if !(10000..=400000).contains(&clk) {
       rtt_target::rprint!("Clock speed is not compatible with this device! | .set_clock()");
       return Err(I2cError::Prog(ProgError::InvalidConfiguration));
     }
